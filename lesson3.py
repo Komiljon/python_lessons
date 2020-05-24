28d366dccb0b95525ee7f544a92c52c3f34d6e36
Kamil <kamil79@yandex.ru>
2020-05-24 22:35:38 +0300

    Срез списков

---
+++ lesson3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+uname = input('Ваше имя?: ')
+uage = input('Ваш возраст?: ')
+
+print('Привет!\nВаше имя {0}\nВам {1} лет'.format(uname, uage))
---
+++ lesson4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+fruits = ['Яблоко','Банан','Груша','Абрикос']
+members = 'Яблоко,Банан,Груша,Абрикос'
+
+print(' - '.join(fruits))
+
+print('Привет Камил'.replace('Камил', 'Камилжан'))
+print(members.split(','))
